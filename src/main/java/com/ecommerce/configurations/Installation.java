1663115cde6b8a6e1b17cc7c32d2529a2afdd86b
marks <marks.senai@gmail.com>
2020-06-11 12:30:55 -0300

    Created entities: City, State

--- src/main/java/com/ecommerce/configurations/Installation.java
+++ src/main/java/com/ecommerce/configurations/Installation.java
@@ -1,8 +1,12 @@
 package com.ecommerce.configurations;
 
 import com.ecommerce.domains.Category;
+import com.ecommerce.domains.City;
 import com.ecommerce.domains.Product;
+import com.ecommerce.domains.State;
 import com.ecommerce.repositories.CategoryRepository;
+import com.ecommerce.repositories.CityRepository;
+import com.ecommerce.repositories.StateRepository;
 import com.ecommerce.resources.ProductRepository;
 import com.ecommerce.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +19,12 @@ import java.util.Arrays;
 public class Installation implements CommandLineRunner {
     @Autowired
     private ProductRepository productRepository;
-
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private StateRepository stateRepository;
+    @Autowired
+    private CityRepository cityRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -40,5 +47,20 @@ public class Installation implements CommandLineRunner {
 
         categoryRepository.saveAll(Arrays.asList(cat1, cat2));
         productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+        State std1 = new State(null, "Santa Catarina");
+        State std2 = new State(null, "Parana");
+
+        City ct1 = new City(null, "Joinville", std1);
+        City ct2 = new City(null, "Florianópolis", std1);
+        City ct3 = new City(null, "Curitiba", std2);
+        City ct4 = new City(null, "Ponta Grossa", std2);
+
+        std1.getCities().addAll(Arrays.asList(ct1, ct2));
+        std2.getCities().addAll(Arrays.asList(ct3, ct4));
+
+        stateRepository.saveAll(Arrays.asList(std1, std2));
+        cityRepository.saveAll(Arrays.asList(ct1, ct2, ct3, ct4));
+
     }
 }
